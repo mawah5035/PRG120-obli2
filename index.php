59a69496ccdb1ed732378f6c9b6d43dc80fda47c
mawah5035 <mariam_wahedi@icloud.com>
2025-10-19 21:33:15 +0200

    registrer klasse

--- index.php
+++ index.php
@@ -8,19 +8,19 @@
 <body>
     <h1>Brukerfunksjoner</h1>
     
-    <h2> Klasse </h2>
+    <h2> Klasse: </h2>
     <ul>
     <li><a href="registereklasse.php"><button>Registere klasse</button></a></li>
     <li><a href="visalleklasser.php"><button>Vis alle klasser</button></a></li>
     <li><a href="slettklasse.php"><button>Slett klasse</button></a></li>
     </ul>
 
-    <h2> Studenter </h2>
+    <h2> Studenter: </h2>
     <ul>
     <li><a href="registerestudenter.php"><button>Registere studenter</button></a></li>
     <li><a href="visallestudneter.php"><button>Vis alle studenter</button></a></li>
     <li><a href="slettstudent.php"><button>Slett student</button></a></li>
     </ul>
-    
+
 </body>
 </html>
---
+++ registrerklasse.php
@@ -0,0 +1,10 @@
+<?php /*registere klasse*/
+?>
+<h3>Registere klasse</h3>
+<form method= "post" action="" id="registereklasseform" name="registereklasseform">
+    Klassekode <input type="text" id="klassekode" name="klassekode" required /> <br/>
+    Klassenavn <input type="text" id="klassenavn" name="klassenavn" required /> <br/>
+    Studimkode <input type="text" id="studiumkode" name="studiumkode" required /> <br/> 
+    <input type="submit" value= "Registrer klasse" id= "registrerKlasseKnapp"name="registrerKlasseKnapp"/>
+    <input type="reset" value="Nullstill" id="nullstill" name= "nullstill" /> <br />
+</form>
