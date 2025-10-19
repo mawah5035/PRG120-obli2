6c8133204237a03dec0e5a08222f8ebbc6c2c931
mawah5035 <mariam_wahedi@icloud.com>
2025-10-19 22:29:54 +0200

    feilretting

--- db-tilkobling.php
+++ db-tilkobling.php
@@ -4,7 +4,7 @@ $user ="mawah5035";
 $pass = "5128mawah5035";
 $db = "mawah5035";
 
-$conn= myspli_connect($host, $user, $pass, $db);
+$conn= mysqli_connect($host, $user, $pass, $db);
 
 if($conn->connect_error) {
     die("Tilkoblingsfeil:" . $conn->connect_error);
