5bbd01161f89fc2ffbe8e15be59cf56c966bee81
mawah5035 <mariam_wahedi@icloud.com>
2025-10-19 20:31:31 +0200

    lister

--- index.php
+++ index.php
@@ -5,12 +5,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PRG120 obligatorisk arbeidskrav 2</title>
 </head>
+<body>
+    <h1>Brukerfunksjoner</h1>
+    
+    <h2> Klasse </h2>
+    <ul>
+    <li><a href="registereklasse.php"><button>Registere klasse</button></a></li>
+    <li><a href="visalleklasser.php"><button>Vis alle klasser</button></a></li>
+    <li><a href="slettklasse.php"><button>Slett klasse</button></a></li>
+    </ul>
 
     <div>
-    <a href="index.php"><button>Forside</button></a>
-    <a href="registereklasse.php"><button>Registere klasse</button></a>
-    <a href="visalleklasser.php"><button>Vis alle klasser</button></a>
-    <a href="slettklasse.php"><button>Slett klasse</button></a>
+    
+    
     <a href="registerestudenter.php"><button>Registere studenter</button></a>
     <a href="visallestudneter.php"><button>Vis alle studenter</button></a>
     <a href="slettstudent.php"><button>Slett student</button></a>
