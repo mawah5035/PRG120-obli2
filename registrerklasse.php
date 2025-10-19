1538d4321e1927dc91a8b4fa9f0a0c53121a4028
mawah5035 <mariam_wahedi@icloud.com>
2025-10-19 22:14:50 +0200

    reg klasse

--- registrerklasse.php
+++ registrerklasse.php
@@ -3,12 +3,35 @@
 
 <h3>Registere klasse</h3>
 
-<form method= "post" action="" id="registereklasseform" name="registereklasseform">
+<form method="post" action="" id="registrerKlasseForm" name="registrerKlasseForm">
     Klassekode <input type="text" id="klassekode" name="klassekode" required /> <br/>
     Klassenavn <input type="text" id="klassenavn" name="klassenavn" required /> <br/>
-    Studimkode <input type="text" id="studiumkode" name="studiumkode" required /> <br/> 
-    <input type="submit" value= "Registrer klasse" id= "registrerKlasseKnapp"name="registrerKlasseKnapp"/>
+    Studiumkode <input type="text" id="studiumkode" name="studiumkode" required /> <br/> 
+    <input type="submit" value= "Registrer klasse" id= "registrerKlasseKnapp" name="registrerKlasseKnapp"/>
     <input type="reset" value="Nullstill" id="nullstill" name= "nullstill" /> <br />
 </form>
 
+<?php
+if (isset($_POST["registrerKlasseForm"]))
+{
+    $klassekode = $_POST["Klassekode"];
+    $klassenavn = $_POST["Klassenavn"];
+    $studiumkode = $_POST["Studiumkode"];
 
+    if (!$klassekode || !$klassenavn || !$studiumkode)
+    {
+        print("Alle felt m&aring; fylles ut");
+    }
+    else
+    {
+        include("db-tilkobling.php"); 
+
+        $sqlSetning= "INSERT INTO klasse (klassekode, klassenavn, studiumkode)
+        VALUES ('$klassekode', '$klassenavn', '$studiumkode');
+        mysqli_query(db,$sqlSetning) or die ("Ikke mulig &aring; registrere data i databasen");
+
+        print ("F&oslash;lgende klasse er n&aring; registrert: $klassekode $klassenavn $studiumkode");
+
+    }
+}
+?>
