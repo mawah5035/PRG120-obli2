91dc2972f5bb7f6ff63b83659a90f7388ae432ad
mawah5035 <mariam_wahedi@icloud.com>
2025-10-20 14:04:08 +0200

    n

--- slettstudent.php
+++ slettstudent.php
@@ -1,16 +1,23 @@
 <?php
-include("db-tilkobling.php");
+include("db-tilkobling.php"); // kobler til databasen
 
-if (isset($_GET['slett'])) {
-    $brukernavn = $_GET['slett'];
+<h3>Slett student</h3>
 
+if (isset($_GET['brukernavn'])) {
+    $brukernavn = $_GET['brukernavn'];
+
+    // Bruk prepared statement for sikkerhet
     $stmt = mysqli_prepare($db, "DELETE FROM student WHERE brukernavn = ?");
     mysqli_stmt_bind_param($stmt, "s", $brukernavn);
+
     if (mysqli_stmt_execute($stmt)) {
         echo "<p style='color:green;'>Studenten '$brukernavn' er slettet.</p>";
     } else {
         echo "<p style='color:red;'>Feil ved sletting: " . mysqli_error($db) . "</p>";
     }
+
     mysqli_stmt_close($stmt);
+} else {
+    echo "<p style='color:red;'>Ingen brukernavn spesifisert for sletting.</p>";
 }
 ?>
