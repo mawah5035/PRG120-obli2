a931170098231930726f26da8fb856e1d3db8370
mawah5035 <mariam_wahedi@icloud.com>
2025-10-20 13:24:59 +0200

    registere student

--- registrerstudent.php
+++ registrerstudent.php
@@ -9,7 +9,7 @@ include("db-tilkobling.php");
     Fornavn: <input type="text" id="fornavn" name="fornavn" required /> <br/>
     Etternavn: <input type="text" id="etternavn" name="etternavn" required /> <br/>
 
-    klassekode: 
+    Klassekode: 
     <select name="klassekode" id="klassekode" required>
         <option value="">-- Velg klasse --</option>
         <?php
@@ -22,7 +22,6 @@ include("db-tilkobling.php");
             echo "<option value='$klassekode'>$klassekode - $klassenavn</option>";
         }
         ?>
-
     </select>
     <br/>
     <input type="submit" value="Registrer student" id="registrerStudentKnapp" name="registrerStudentKnapp"/>
@@ -30,39 +29,30 @@ include("db-tilkobling.php");
 </form>
 
 <?php
-if (isset($_POST["registrerStudentKnapp"])) 
-    {
-    $brukernavn= trim($_POST["brukernavn"]);
-    $fornavn= trim($_POST["fornavn"]);
-    $etternavn= trim($_POST["etternavn"]);
-    $klassekode= trim($_POST["klassekode"]);
+if (isset($_POST["registrerStudentKnapp"])) {
+    $brukernavn = trim($_POST["brukernavn"]);
+    $fornavn = trim($_POST["fornavn"]);
+    $etternavn = trim($_POST["etternavn"]);
+    $klassekode = trim($_POST["klassekode"]);
 
-    if (empty($brukernavn) || empty($fornavn) || empty($etternavn) || empty($klassekode))
-    {
-        echo "<p style='color:red;'> Alle felt m&aring; fylles ut!</p>";
-    }
-    else
-    {
-        $sjekk= "SELECT * FROM student WHERE brukernavn='$brukernavn'";
-        $resultat= mysqli_query($db, $sjekk);
+    if (empty($brukernavn) || empty($fornavn) || empty($etternavn) || empty($klassekode)) {
+        echo "<p style='color:red;'>Alle felt må fylles ut!</p>";
+    } else {
+        $sjekk = "SELECT * FROM student WHERE brukernavn='$brukernavn'";
+        $resultat = mysqli_query($db, $sjekk);
 
-        if (mysqli_num_rows($resultat) > 0)
-        {
-            echo "<p style='color:red;'> Feil: Brukernavnet <strong>$brukernavn</strong> finnes allerede!</p>";
-        }
-        else
-        {
-            $sqlSetning= "INSERT INTO student (brukernavn, fornavn, etternavn, klassekode)
-                          VALUES ('$brukernavn', '$fornavn', '$etternavn', '$klassekode')";
-            if (mysqli_query($db, $sqlSetning))
-            {
-                echo "<p style='color:green;'>F&oslash;lgende student er n&aring; registrert: <strong>$brukernavn, $fornavn, $etternavn, $klassekode</strong></p>";
-            }
-            else
-            {
-                echo "<p style='color:red;'> Feil ved registrering: " . mysqli_error($db) . "</p>";
+        if (mysqli_num_rows($resultat) > 0) {
+            echo "<p style='color:red;'>Feil: Brukernavnet <strong>$brukernavn</strong> finnes allerede!</p>";
+        } else {
+            $sqlSetning = "INSERT INTO student (brukernavn, fornavn, etternavn, klassekode)
+                           VALUES ('$brukernavn', '$fornavn', '$etternavn', '$klassekode')";
+            if (mysqli_query($db, $sqlSetning)) {
+                echo "<p style='color:green;'>Følgende student er nå registrert: 
+                      <strong>$brukernavn, $fornavn, $etternavn, $klassekode</strong></p>";
+            } else {
+                echo "<p style='color:red;'>Feil ved registrering: " . mysqli_error($db) . "</p>";
             }
         }
     }
-
-    ?>
+}
+?>
